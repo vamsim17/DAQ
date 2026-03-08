9c1989f9d87d22ae97db07598eba25cd449a0c3c
vamsim17 <vamsimarrapu17@gmail.com>
2026-03-08 18:36:43 -0500

    added iso_timestamp, changed some of load sensor specs, added generate sensors and payload

--- synthetic_data/syn_data.py
+++ synthetic_data/syn_data.py
@@ -3,13 +3,42 @@ import random
 import csv
 from datetime import datetime, timezone
 
+def iso_timestamp():
+    return datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
+
 def load_sensor_specs():
     specs = {}
-    with open ("docs/sensor_specs.csv") as f:
+    with open ("docs/sensor_specs.csv", newline = '') as f:
         reader = csv.DictReader
         for row in reader:
-            dtype = row["data_type"].strip()
-            min = int((row["min_range"]) if dtype == "integer" else float(row["min_range"]))
-            max = int((row["max_range"]) if dtype == "integer" else float(row["max_range"]))
-            specs[row["sensor_name"].strip()] = (min, max, dtype)
+            specs[row["sensor_name"]] = {
+                "type": row["data_type"],
+                "min": float(row["min_range"]),
+                "max": float(row["max_range"])
+            }
     return specs
+    
+def generate_sensors(specs):
+    sensors = {}
+    for sensor, spec in specs.items():
+        value = random.uniform(spec["min"], spec["max"])
+        if spec["value"] == "int":
+            value = int(value)
+        else:
+            value = round(value, 2)
+    return sensors
+
+def generate_payload(specs):
+    payload = {
+        "timestamp": iso_timestamp(),
+        "session_id": "auto_" + datetime.now().strftime("%Y%m%d_%H%M%S"),
+        "vehicle_id": "FSAE_2022_001",
+        "sensors": generate_sensors(specs),
+        "telemetry_metadata": {
+            "packet_id": f"pkt_{int(datetime.now().timestamp()*1000)}",
+            "sample_rate_hz": 100,
+            "daq_version": "v2.2.1"
+        }
+    }
+    return payload
+    
